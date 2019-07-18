bb872351197c0be8276007910820356700926138
Nik <nik.ignition.work@gmail.com>
2019-07-18 22:52:49 +0300

    init commit

---
+++ src/main/java/data/ConnectionPool.java
@@ -0,0 +1,41 @@
+package data;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+public class ConnectionPool {
+    private static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/tester_app_prod_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String DB_USER = "admin";
+    private static final String DB_PASSWORD = "pa44word";
+    private static final int CONN_MIN_POOL_SIZE = 5;
+    private static final int CONN_MAX_POOL_SIZE = 10;
+    private static final int MAX_NUMBER_OF_PREPARED_STATEMENTS = 100;
+
+    private BasicDataSource ds = new BasicDataSource();
+
+    private static class DataSourceHolder {
+        private static final ConnectionPool INSTANCE = new ConnectionPool();
+    }
+
+    private ConnectionPool() {
+        ds.setUrl(DB_URL);
+        ds.setUsername(DB_USER);
+        ds.setPassword(DB_PASSWORD);
+        ds.setMinIdle(CONN_MIN_POOL_SIZE);
+        ds.setMaxIdle(CONN_MAX_POOL_SIZE);
+        ds.setMaxOpenPreparedStatements(MAX_NUMBER_OF_PREPARED_STATEMENTS);
+        ds.setDriverClassName(DRIVER_CLASS_NAME);
+    }
+
+    public static ConnectionPool getInstance() {
+        return DataSourceHolder.INSTANCE;
+    }
+
+    public BasicDataSource getDs() {
+        return ds;
+    }
+
+    public void setDs(BasicDataSource ds) {
+        this.ds = ds;
+    }
+}
---
+++ src/scripts/scripts.sql
@@ -0,0 +1,33 @@
+CREATE SCHEMA `tester_app_prod_db` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `tester_app_prod_db`.`user` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `user_email` VARCHAR(254) NOT NULL,
+  `user_password` VARCHAR(25) NOT NULL,
+  `user_role` INT NOT NULL,
+  `user_name` VARCHAR(75) NOT NULL,
+  `user_surname` VARCHAR(150) NOT NULL,
+  `user_have_tests` INT NULL,
+  PRIMARY KEY (`user_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COMMENT = 'database for user info ';
+
+CREATE TABLE `tester_app_prod_db`.`user_roles` (
+  `id_user_roles` INT NOT NULL AUTO_INCREMENT,
+  `role_name` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`id_user_roles`));
+
+INSERT INTO `tester_app_prod_db`.`user_roles`
+(`id_user_roles`,
+`role_name`)
+VALUES
+(1,
+'ADMIN');
+
+INSERT INTO `tester_app_prod_db`.`user_roles`
+(`id_user_roles`,
+`role_name`)
+VALUES
+(2,
+'USER');
